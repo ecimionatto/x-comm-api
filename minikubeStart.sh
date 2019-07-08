9a28874c2cd907eb3306b872715d29c155d4a9fa
ecimio <edson.cimionatto@nike.com>
2019-07-08 09:41:44 -0700

    accepting cors

--- minikubeStart.sh
+++ minikubeStart.sh
@@ -5,5 +5,7 @@ kubectl create -f ./kubMongo.yaml
 kubectl create -f ./kubMongoService.yaml
 kubectl create -f ./kubApi.yaml
 kubectl create -f ./kubApiService.yaml
+kubectl create -f ./kubUx.yaml
+kubectl create -f ./kubUxService.yaml
 kubectl get services
 minikube ip
--- src/main/java/com/ecimio/xcomm/endpoint/DashboardController.java
+++ src/main/java/com/ecimio/xcomm/endpoint/DashboardController.java
@@ -3,6 +3,7 @@ package com.ecimio.xcomm.endpoint;
 import com.ecimio.xcomm.model.Dashboard;
 import com.ecimio.xcomm.repo.DashboardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import reactor.core.publisher.Flux;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/dashboard", consumes = APPLICATION_JSON_VALUE)
 public class DashboardController {
