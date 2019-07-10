e4842e9a703e109f0ecae25fd23fbc3b9bf016a8
ecimio <edson.cimionatto@nike.com>
2019-07-09 17:59:10 -0700

    multiple channels

--- src/main/java/com/ecimio/xcomm/service/SlackCommand.java
+++ src/main/java/com/ecimio/xcomm/service/SlackCommand.java
@@ -7,14 +7,21 @@ import com.github.seratch.jslack.api.webhook.Payload;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.List;
 
 @Component
 public class SlackCommand implements MessageCommand {
 
     public void send(final Communication communication, final Settings settings) {
+        List.of(communication.getSlackTo()
+                .orElseThrow(IllegalStateException::new)
+                .split(","))
+                .forEach(channel -> sendToChannel(communication, settings, channel));
+    }
 
+    private void sendToChannel(final Communication communication, final Settings settings, final String slackTo) {
         Payload payload = Payload.builder()
-                .channel(communication.getSlackTo().orElseThrow(IllegalStateException::new))
+                .channel(slackTo)
                 .username("xcomm")
                 .iconEmoji(":smile_cat:")
                 .text(communication.getMessage())
